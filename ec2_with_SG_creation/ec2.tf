5d03a4647f884ea5d7cc9e1c337616332dbe1cbc
learndaws <learndaws@gmail.com>
2024-07-04 13:33:39 +0530

    all

--- ec2_with_SG_creation/ec2.tf
+++ ec2_with_SG_creation/ec2.tf
@@ -1,5 +1,5 @@
 resource "aws_instance" "TF_Second_EC2" {
-  ami           = "ami-0f3c7d07486cad139"
+  ami           = "ami-0b4f379183e5706b9"
   instance_type = var.instance_type
 
   tags = {
--- ec2_with_SG_creation/sg.tf
+++ ec2_with_SG_creation/sg.tf
@@ -8,18 +8,20 @@ resource "aws_security_group" "TF_Second_SG" {
   }
 }
 
-resource "aws_vpc_security_group_ingress_rule" "TF_Second_SG_ingress_allow_all" {
-  security_group_id = aws_security_group.TF_Second_SG.id
-  cidr_ipv4         = "0.0.0.0/0"
-  ip_protocol       = "-1"
+resource "aws_security_group_rule" "TF_Second_SG_ingress_allow_all" {
+  type              = "ingress"
   from_port         = 0
   to_port           = 0
+  protocol          = "-1"
+  cidr_blocks       = ["0.0.0.0/0"]
+  security_group_id = aws_security_group.TF_Second_SG.id
 }
 
-resource "aws_vpc_security_group_egress_rule" "TF_Second_SG_egress_allow_all" {
-  security_group_id = aws_security_group.TF_Second_SG.id
-  cidr_ipv4         = "0.0.0.0/0"
-  ip_protocol       = "-1" # semantically equivalent to all ports
+resource "aws_security_group_rule" "TF_Second_SG_egress_allow_all" {
+  type              = "egress"
   from_port         = 0
   to_port           = 0
+  protocol          = "-1"
+  cidr_blocks       = ["0.0.0.0/0"]
+  security_group_id = aws_security_group.TF_Second_SG.id
 }
